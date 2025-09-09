0ef0c45a333abda756dee438b9f954463224d24b
irinbear <irinb3ar@gmail.com>
2025-09-09 10:40:50 +0700

    done memset

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,1 +1,9 @@
 #include "libft.h"
+void bzero(void *s, size_t n) {
+
+}
+
+#include <strings.h>
+int main() {
+
+}
--- ft_memset.c
+++ ft_memset.c
@@ -1,2 +1,32 @@
 #include "libft.h"
 
+void *ft_memset(void *b, int c, size_t len) {
+    unsigned char   *tmp_ptr;
+    tmp_ptr =   (unsigned    char *)b;
+    while(len > 0) {
+        *tmp_ptr =   (unsigned char) c;
+        tmp_ptr++;
+        len--;
+    }
+    return b;
+}
+
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     char str[50] = "Hello 42bangkok";
+//     char ft_str[50] = "Hello 42bangkok";
+//     printf("\nBefore memset(): %s\n", str);
+//     printf("\nBefore ft_memset(): %s\n", ft_str);
+
+//     // Fill 8 characters starting from str[13] with '.'
+//     memset(str + 13, '.', 8*sizeof(char));
+//     ft_memset(ft_str + 13, '.', 8*sizeof(char));
+
+//     printf("After memset():  %s", str);
+//     printf("\nAfter ft_memset():  %s", ft_str);
+//     return 0;
+// }
