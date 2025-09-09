20c212ddc86e9bfad511c29a4d180eb556c2f3f1
irinbear <irinb3ar@gmail.com>
2025-09-09 10:56:46 +0700

    done memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,26 +1,42 @@
 #include "libft.h"
 
-char	*ft_memcpy(char *dest, char *src)
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
-	int	i;
+    unsigned    char    *tmp_dst;
+    unsigned    char    *tmp_src;
 
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+    if(dst == (void *)0 &&  src == (void *)0)
+        return dst;
+    tmp_dst = (unsigned char *)dst;
+    tmp_src = (unsigned char *)src;
+
+    while(n>0) {
+        *tmp_dst = *tmp_src;
+        tmp_dst++;
+        tmp_src++;
+        n--;
+    }
+    return dst;
 }
 
 // #include <stdio.h>
+// #include <string.h>
 
-// int	main(void)
+// int main()
 // {
-// 	char *src = "Hello, 42bkk!";
-// 	char dest[100];
+//     char str1[] = "42bkk";
+//     char str2[6] = "";
+//     char ft_str1[] = "42bkk";
+//     char ft_str2[6] = "";
 
-// 	ft_strcpy(dest, src);
-// 	printf("%s", dest);
+//     // Copies contents of str2 to str1
+//     memcpy(str2, str1, sizeof(str1));
+//     ft_memcpy(ft_str2, ft_str1, sizeof(ft_str1));
+
+//     printf("str2 after memcpy:");
+//     printf("%s",str2);
+//     printf("\nft_str2 after memcpy:");
+//     printf("%s",ft_str2);
+
+//     return 0;
 // }
