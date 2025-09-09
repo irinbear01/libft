b62be44e4aef35b8e97fcf481b191155888a0fca
irinbear <irinb3ar@gmail.com>
2025-09-09 16:07:44 +0700

    done strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,1 +1,38 @@
 #include "libft.h"
+
+char *ft_strrchr(const char *s, int c)
+{
+    unsigned int    i;
+    char    *res;
+    char    new_c;
+
+    new_c = (char) c;
+    res = NULL;
+    i = 0;
+
+    while (s[i])
+    {
+        if(s[i] == new_c)
+            res = ((char *) &s[i]);
+        i++;
+    }
+    if (s[i] == new_c)
+        res = ((char *) &s[i]);
+    return (res);
+}
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//    const char *str = "hello this is test for strrchr";
+//    char ch = 't';
+//    const char *ft_str = "hello this is test for ft_strrchr";
+//    char ft_ch = 't';
+   
+//    char *p = strrchr(str, ch);
+//    char *ft_p = ft_strrchr(str, ch);
+
+//    printf("strrchr String starting from '%c' is: %s\n", ch, p);
+//    printf("ft_strrchr String starting from '%c' is: %s\n", ft_ch, ft_p);
+// }
